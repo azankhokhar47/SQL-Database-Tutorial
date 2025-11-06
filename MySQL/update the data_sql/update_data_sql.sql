8a42932168dfe987414798152f972b40237c6e0e
Azan khokhar <azankhokhar203@gmail.com>
2025-11-06 17:13:53 +0500

    Add files via upload

---
+++ MySQL/update the data_sql/update_data_sql.sql
@@ -0,0 +1,6 @@
+USE startersql;
+-- UPDATE users SET email = 'azankhokhar@email.com' WHERE id = 1;
+-- UPDATE users SET name = 'ali' WHERE id = 2;
+-- UPDATE users SET salary = '700000' WHERE id = 3;
+-- UPDATE users SET salary = salary + 10000 WHERE salary < 60000;
+SELECT * FROM users;
