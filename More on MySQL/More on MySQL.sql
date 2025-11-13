6d0689cb7866edca8049e030b7536b38cb960794
Azan khokhar <azankhokhar203@gmail.com>
2025-11-13 12:19:50 +0500

    Last changes

---
+++ More on MySQL/More on MySQL.sql
@@ -0,0 +1,42 @@
+USE startersql;
+
+SELECT * FROM users;
+
+-- ALTER TABLE users
+-- ADD COLUMN city VARCHAR(100);
+
+-- SELECT * FROM users
+-- ORDER BY id
+-- LIMIT 5 OFFSET 10;
+
+-- SELECT * FROM users
+-- ORDER BY id
+-- LIMIT 5;
+
+-- SELECT * FROM users
+-- ORDER BY id
+-- LIMIT 5 OFFSET 10;
+
+-- SELECT * FROM users
+-- ORDER BY id
+-- LIMIT 10, 5;
+
+-- SELECT * FROM users WHERE name LIKE '_a%'
+
+-- SELECT * FROM users WHERE name LIKE 'F%'
+
+-- SELECT * FROM users WHERE name LIKE '_a_h%'
+
+-- SELECT DISTINCT gender FROM users;
+
+-- SELECT gender FROM users;
+
+-- SELECT * FROM addresses;
+
+-- TRUNCATE TABLE addresses;
+
+-- ALTER TABLE users
+-- CHANGE COLUMN city location VARCHAR(150);
+
+-- ALTER TABLE users
+-- MODIFY COLUMN salary BIGINT;
